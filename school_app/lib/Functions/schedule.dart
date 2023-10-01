ec9d8b0d812a0b2cf2fca5f0d0181c800667b009
Kyriakos Nikoletti <kyriakos2008nic@gmail.com>
2023-10-01 17:32:45 +0300

    May load faster
    It first get basic data from cache

--- school_app/lib/Functions/schedule.dart
+++ school_app/lib/Functions/schedule.dart
@@ -1,49 +1,96 @@
 import 'package:html/parser.dart' as parser;
 import 'package:requests/requests.dart';
 import 'package:html/dom.dart' as dom;
+import 'package:school_app/Functions/login.dart';
+import 'package:school_app/Screens/login_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:school_app/Screens/main_screen.dart';
 
 List<List<String>> daySchedule = [];
 
 Future<void> schedule() async {
-  var schedule = await Requests.get('http://81.4.170.42/~lyk-latsia-lef/epiloges/mathitisp2.php');
-  
-  if (schedule.statusCode != 200) {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  daySchedule = [];
+  var schedule = await Requests.get(
+      'http://81.4.170.42/~lyk-latsia-lef/epiloges/mathitisp2.php');
+
+  if (schedule.statusCode == 200) {
+    dom.Document document = parser.parse(schedule.body);
+
+    // Find all table elements with class 'myTable'
+    List<dom.Element> tables = document.querySelectorAll('.myTable');
+
+    tables.forEach((table) {
+      // Find all row elements in the table
+      List<dom.Element> rows = table.querySelectorAll('tr');
+
+      rows.forEach((row) {
+        // Find all cell elements in the row
+        List<dom.Element> cells = row.querySelectorAll('td');
+
+        // Check if the row has more than one column
+        if (cells.length > 1) {
+          // Loop through each cell element, starting from the second column (index 1)
+          cells.skip(1).forEach((cell) {
+            // If the column list does not exist, create it
+            if (daySchedule.length <= cells.indexOf(cell) - 1) {
+              daySchedule.add([]);
+            }
+
+            // Check if the cell text is equal to '// ()'
+            if (cell.text != 'x//  () ' && cell.text != '***//  () ') {
+              //&& cell.text != '//  () '
+              // Append the cell text to the column list
+              daySchedule[cells.indexOf(cell) - 1].add(cell.text);
+            }
+          });
+        }
+      });
+    });
+    String dayScheduleJson = jsonEncode(daySchedule);
+    prefs.setString("dayScheduleKey", dayScheduleJson);
+    print('done schedule');
     return;
+  } else {
+    print('schedule failed');
+  }
+}
+
+scheduleget() async {
+  print('xekina to prama');
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  // Retrieve your JSON string using the getString method with the same key
+  String? dayScheduleJson = prefs.getString("dayScheduleKey");
+
+  // Decode your JSON string back into a Dart object
+  List<List<String>> daySchedule = (jsonDecode(dayScheduleJson!) as List)
+      .map((element) => List<String>.from(element))
+      .toList();
+
+  scheduleVerify();
+  return daySchedule;
+}
+
+detailsGet() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  cell1 = prefs.getString('cell1');
+  cell2 = prefs.getString('cell2');
+  cell3 = prefs.getString('cell3');
+  cell4 = prefs.getString('cell4');
+  nameOnly = prefs.getString('nameOnly');
+  return;
+}
+
+Future<void> scheduleVerify() async {
+  await login(null, null);
+  await schedule();
+  setState() {
+    // Update your ListView here
+    parsedDaySchedule = parseSubjects(daySchedule);
   }
 
-  dom.Document document = parser.parse(schedule.body);
-
-  // Find all table elements with class 'myTable'
-  List<dom.Element> tables = document.querySelectorAll('.myTable');
-
-  tables.forEach((table) {
-    // Find all row elements in the table
-    List<dom.Element> rows = table.querySelectorAll('tr');
-
-    rows.forEach((row) {
-      // Find all cell elements in the row
-      List<dom.Element> cells = row.querySelectorAll('td');
-
-      // Check if the row has more than one column
-      if (cells.length > 1) {
-        // Loop through each cell element, starting from the second column (index 1)
-        cells.skip(1).forEach((cell) {
-          // If the column list does not exist, create it
-          if (daySchedule.length <= cells.indexOf(cell) - 1) {
-            daySchedule.add([]);
-          }
-
-          // Check if the cell text is equal to '// ()'
-          if (cell.text != 'x//  () ' && cell.text != '//  () ' && cell.text != '***//  () ') {
-            // Append the cell text to the column list
-            daySchedule[cells.indexOf(cell) - 1].add(cell.text);
-          }
-        });
-      }
-    });
-  });
-
-  print('done schedule');
+  ;
 }
 
 class SchoolSubject {
@@ -108,5 +155,3 @@ List<List<SchoolSubject>> parseSubjects(List<List<String>> daySchedule) {
     }).toList();
   }).toList();
 }
-
-
